1f9423836af49cbb91e1181489b3f6cbb7fdbe54
Daniel Ramos <danielartramos@gmail.com>
2026-07-01 15:21:55 -0700

    Test

---
+++ LearningJava/Chapter1/HideAndSeek.java
@@ -0,0 +1,17 @@
+public class HideAndSeek {
+
+
+  // some cool things to output
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two..."); 
+    System.out.println("One...");
+    System.out.println("Ready or not, here I come!");
+
+  }
+}
+
+/*
+test comment 
+*/
